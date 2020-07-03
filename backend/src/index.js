5172fcbabccdb314288b025808ff7217388b3789
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-07-03 00:36:23 -0300

    Finalização do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,16 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+const routes = require('./routes');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Hello world' });
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
